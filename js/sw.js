d8085536c37b25fae4e910f120bd7f19707e9586
Gicela Morales <gicela@mexicanwave.com>
2018-03-20 15:08:32 +0000

    implements caching

--- js/sw.js
+++ js/sw.js
@@ -1,22 +1,74 @@
+(function() {
+  'use strict';
 
-var CACHE_NAME = 'restaurant-static-v1';
-var urlsToCache = [
-  '/',
-  '/index.html',
-  '/restaurant.html',
-  '/css/styles.css',
-  '/js/',
-  '/images/',
-  '/data/'
-];
+  var filesToCache = [
+    '/',
+    '../js/main.js', 
+    '../js/',
+    '../css/styles.css', 
+    '../images/', 
+    '../data/',
+    '../restaurant.html',
+    '../index.html'
+  ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
+  var staticCacheName = 'restaurant-cache-v3';
+
+  self.addEventListener('install', function(event) {
+    console.log('Attempting to install service worker and cache static assets');
+    event.waitUntil(
+      caches.open(staticCacheName)
       .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
+        return cache.addAll(filesToCache);
       })
-  );
-});
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then(function(response) {
+          if (response.status === 404) {
+            return caches.match('404.html');
+          }
+          return caches.open(staticCacheName).then(function(cache) {
+            if (event.request.url.indexOf('test') < 0) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          });
+        });
+      }).catch(function(error) {
+        console.log('Error, ', error);
+        return caches.match('offline.html');
+      })
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    console.log('Activating new service worker...');
+
+    var cacheWhitelist = [staticCacheName];
+
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+
+})();
+
+
