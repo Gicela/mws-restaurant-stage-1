4a75dcac894b93426382f072c4b444a6a0877553
Gicela Morales <gicela@mexicanwave.com>
2018-04-05 18:04:20 +0100

    now caching to work offline

--- sw.js
+++ sw.js
@@ -1,54 +1,68 @@
-var staticCacheName = 'restaurant-cache-v1';
 
-var filesToCache = [
-    '/',
-    'js/main.js', 
-    'js/dbhelper.js',
-    'js/restaurant_info',
-    'css/styles.css', 
-    'images/1-309_medium_2x.jpg', 
-    'images/1-800_large_2x.jpg',
-    'images/2-309_medium_2x.jpg',
-    'images/2-800_large_2x.jpg',
-    'images/3-309_medium_2x.jpg',
-    'images/3-800_large_2x.jpg',
-    'images/4-309_medium_2x.jpg',
-    'images/4-800_large_2x.jpg',
-    'images/5-309_medium_2x.jpg',
-    'images/5-800_large_2x.jpg',
-    'images/6-309_medium_2x.jpg',
-    'images/6-800_large_2x.jpg',
-    'images/7-309_medium_2x.jpg',
-    'images/7-800_large_2x.jpg',
-    'images/8-309_medium_2x.jpg',
-    'images/8-800_large_2x.jpg',
-    'images/9-309_medium_2x.jpg',
-    'images/9-800_large_2x.jpg',
-    'images/10-309_medium_2x.jpg',
-    'images/10-800_large_2x.jpg',
-    'data/restaurants.json',
-    'restaurant.html',
-    'index.html',
-    'https://maps.googleapis.com/maps/api/js?key=AIzaSyBpl2KDyqIaOiBU-E11ZjwMrH-rHgJNujg&libraries=places&callback=initMap'
-  ];
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('restaurantCache installing…');
+  event.waitUntil(
+    caches.open('restaurantCache').then(function(cache) {
+      return cache.addAll(
+        [
+          '/',
+          'js/main.js', 
+          'js/dbhelper.js',
+          'js/restaurant_info.js',
+          'css/styles.css', 
+          'images/1-309_medium_2x.jpg', 
+          'images/1-800_large_2x.jpg',
+          'images/2-309_medium_2x.jpg',
+          'images/2-800_large_2x.jpg',
+          'images/3-309_medium_2x.jpg',
+          'images/3-800_large_2x.jpg',
+          'images/4-309_medium_2x.jpg',
+          'images/4-800_large_2x.jpg',
+          'images/5-309_medium_2x.jpg',
+          'images/5-800_large_2x.jpg',
+          'images/6-309_medium_2x.jpg',
+          'images/6-800_large_2x.jpg',
+          'images/7-309_medium_2x.jpg',
+          'images/7-800_large_2x.jpg',
+          'images/8-309_medium_2x.jpg',
+          'images/8-800_large_2x.jpg',
+          'images/9-309_medium_2x.jpg',
+          'images/9-800_large_2x.jpg',
+          'images/10-309_medium_2x.jpg',
+          'images/10-800_large_2x.jpg',
+          'data/restaurants.json',
+          'restaurant.html',
+          'index.html'
+        ]
+      );
+    })
+  );
+});
 
 
-  self.addEventListener('install', function(event) {
-    console.log('Attempting to install service worker and cache static assets');
-    event.waitUntil(
-      caches.open(staticCacheName)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(filesToCache);
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
       })
-    );
-  });
+    )).then(() => {
+      console.log('restaurantCache now ready to handle fetches!');
+    })
+  );
+});
 
- 
+
+
 self.addEventListener('fetch', function(event) {
   console.log('Fetch event for ', event.request.url);
   event.respondWith(
-    caches.open('restaurant-cache-v1').then(function(cache) {
+    caches.open('restaurantCache').then(function(cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function(response) {
           cache.put(event.request, response.clone());
@@ -60,20 +74,3 @@ self.addEventListener('fetch', function(event) {
 });
 
 
-  self.addEventListener('activate', function(event) {
-    console.log('Activating new service worker...');
-
-    var cacheWhitelist = [staticCacheName];
-
-    event.waitUntil(
-      caches.keys().then(function(cacheNames) {
-        return Promise.all(
-          cacheNames.map(function(cacheName) {
-            if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName);
-            }
-          })
-        );
-      })
-    );
-  });
